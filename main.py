c0cd042eb4f3f618bb96b473e814636e1b28306c
Sakibul Islam <sak2241@gmail.com>
2020-10-07 03:15:50 +0600

    updated url

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-url = 'https://www.gmail.com'
+url = 'https://www.facebook.com'
 chrome_driver_location = 'F:\\Python Projects\\EmailAutomation\\utilities\\chromedriver'
 
 driver = webdriver.Chrome(executable_path = chrome_driver_location)
